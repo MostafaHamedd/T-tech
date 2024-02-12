f84ddf0ca6e417075a43bfab4e691934066d5bb8
Mostafa Hamed <mostafahamed@wifi-wpa-cw2-140-193-77-9.wifi.umanitoba.ca>
2024-02-12 03:55:23 -0600

    App created

---
+++ T-tech/T-tech/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  T-tech
+//
+//  Created by Mostafa Hamed on 2024-02-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ T-tech/T-tech/T_techApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_techApp.swift
+//  T-tech
+//
+//  Created by Mostafa Hamed on 2024-02-12.
+//
+
+import SwiftUI
+
+@main
+struct T_techApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
