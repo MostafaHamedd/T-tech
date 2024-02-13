722adabe677ff18e45eccfdb029877cc8a0543c8
Mostafa Hamed <mostafahamed@wifi-wpa-cw2-140-193-77-9.wifi.umanitoba.ca>
2024-02-12 19:50:39 -0600

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>
+        Open up App.js to start working on your app lol testing!
+      </Text>
+      <Button style={styles.buttonStyle} title="Press Me!" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    color: "#000",
+  },
+  buttonStyle: {
+    backgroundColor: "blue",
+    borderRadius: 5,
   },
 });
